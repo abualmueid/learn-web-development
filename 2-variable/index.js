09c37652b6b87c2eac0156353f7447c201e82d42
abualmueid <61595430+abualmueid@users.noreply.github.com>
2024-10-01 10:46:21 +0600

    moved to 2-variable folder

---
+++ 2-variable/index.js
@@ -0,0 +1,9 @@
+let name = "Mueid";
+let age = 30;
+let height = 5.9;
+
+console.log(`My name is ${name}`);
+console.log(`My age is ${age}`);
+console.log(`My height is ${height}`);
+
+console.log(typeof height);

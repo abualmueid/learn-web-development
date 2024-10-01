89e51cdad8629a147de783e48e46b805cb3c92b4
abualmueid <61595430+abualmueid@users.noreply.github.com>
2024-10-01 10:45:44 +0600

    modified file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log('I love Javascript');
-alert('This is an alert!');
-window.alert('This is another alert!');
+// alert('This is an alert!');
+// window.alert('This is another alert!');
+
+document.getElementById('hasan').textContent = "Hello Mueid";
+document.getElementById('jahid').innerHTML = "Hello Jisan";
